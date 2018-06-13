5d17e34f46a0219e4c448dd5def65dd13c34d35d
ltt19921124 <ltt19921124@163.com>
2018-06-13 22:02:49 +0800

    K
    K

---
+++ NiuCoderLesson_1/src/lesson5/Code_01_KMP.java
@@ -0,0 +1,55 @@
+package lesson5;
+
+public class Code_01_KMP {
+
+	public static int getIndexOf(String s, String m) {
+		if (s == null || m == null || m.length() < 1 || s.length() < m.length()) {
+			return -1;
+		}
+		char[] ss = s.toCharArray();
+		char[] ms = m.toCharArray();
+		int si = 0;
+		int mi = 0;
+		int[] next = getNextArray(ms);
+		while (si < ss.length && mi < ms.length) {
+			if (ss[si] == ms[mi]) {
+				si++;
+				mi++;
+			} else if (next[mi] == -1) {
+				si++;
+			} else {
+				mi = next[mi];
+			}
+		}
+		return mi == ms.length ? si - mi : -1;
+	}
+
+	public static int[] getNextArray(char[] str2) {
+		if (str2.length == 1) {
+			return new int[] { -1 };
+		}
+		int[] next = new int[str2.length];
+		next[0] = -1;
+		next[1] = 0;
+		int pos = 2;
+		int cn = 0;
+		while (pos < next.length) {
+			if (str2[pos - 1] == str2[cn]) {
+				next[pos++] = ++cn;
+			} else if (cn > 0) {
+				cn = next[cn];
+			} else {
+				next[pos++] = 0;
+			}
+		}
+		return next;
+	}
+
+	public static void main(String[] args) {
+		String str = "abcabcababaccc";
+		String match = "ababa";
+		System.out.println(getIndexOf(str, match));
+
+	}
+
+}
